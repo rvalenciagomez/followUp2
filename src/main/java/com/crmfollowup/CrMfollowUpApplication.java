445b59b4208da3b9f4670f3bf26d2224bef411f6
rvalenciagomez <robertovalenciagomez@gmail.com>
2016-08-29 05:26:31 -0500

    dddd

--- src/main/java/com/crmfollowup/CrMfollowUpApplication.java
+++ src/main/java/com/crmfollowup/CrMfollowUpApplication.java
@@ -4,18 +4,11 @@ package com.crmfollowup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class CrMfollowUpApplication extends SpringBootServletInitializer {
-  
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-      return application.sources(CrMfollowUpApplication.class);
-  }
+public class CrMfollowUpApplication {
   
 	public static void main(String[] args) {
 		SpringApplication.run(CrMfollowUpApplication.class, args);
